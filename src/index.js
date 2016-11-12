332fafc42e3d006d68f0a2ba839715e35214bbe6
Igor Ilić <ilicigor@outlook.com>
2016-11-12 18:05:33 +0100

    Index testing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-import numeral from 'numeral';
 import './index.css';
+import {getUsers} from './api/usersApi';
 
-const courseValue = numeral(1000).format('$0,0.00');
-console.log(`I would pay ${courseValue} for this course.`);
+getUsers()
+  .then(result => {
+    let userBody = "";
+
+    result.map(user => {
+      userBody += `<tr>
+        <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+        <td>${user.id}</td>
+        <td>${user.firstName}</td>
+        <td>${user.lastName}</td>
+        </tr>`
+    });
+    global.document.getElementById('users').innerHTML = userBody;
+  });
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,5 +1,6 @@
 import {expect} from 'chai';
 import jsdom from 'jsdom';
+import cheerio from 'cheerio';
 import fs from 'fs';
 
 describe('Init test', () => {
@@ -7,15 +8,33 @@ describe('Init test', () => {
     expect(true).to.equal(true);
   });
 });
-
+beforeEach(() => {
+  const index = fs.readFileSync('./src/index.html', "utf-8");
+  return index;
+})
 describe('index.html', () => {
-  it('should say hello', (done) => {
+  it('should say h1 says sers', (done) => {
     const index = fs.readFileSync('./src/index.html', "utf-8");
     jsdom.env(index, function(err, window) {
       const h1 = window.document.getElementsByTagName('h1')[0];
-      expect(h1.innerHTML).to.equal('Hello World!');
+      expect(h1.innerHTML).to.equal('Users');
       done();
       window.close();
     });
   });
+  it('should have td with id users', () => {
+    const index = fs.readFileSync('./src/index.html', "utf-8");
+    const $ = cheerio.load(index);
+    const users = $('tbody').attr('id');
+    const userList = $('thead th').length;
+    expect(users).to.equal('users');
+    expect(userList).to.equal(4);
+    // done();
+
+    // jsdom.env(index, function(err, window) {
+    //   const users = window.document.getElementsById('users')[0];
+    //   
+    //   window.close();
+    // })
+  })
 });
