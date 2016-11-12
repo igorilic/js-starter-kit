0f8dd654e21e3e262b9956c46f5bd104eb70c7ef
Igor Ilić <ilicigor@outlook.com>
2016-11-12 20:19:26 +0100

    mock api

--- src/index.spec.js
+++ src/index.spec.js
@@ -3,32 +3,39 @@ import jsdom from 'jsdom';
 import cheerio from 'cheerio';
 import fs from 'fs';
 
+let index;
+let $;
+
 describe('Init test', () => {
   it('should pass', () => {
     expect(true).to.equal(true);
   });
 });
-beforeEach(() => {
-  const index = fs.readFileSync('./src/index.html', "utf-8");
-  return index;
-})
+
 describe('index.html', () => {
-  it('should say h1 says sers', (done) => {
-    const index = fs.readFileSync('./src/index.html', "utf-8");
-    jsdom.env(index, function(err, window) {
-      const h1 = window.document.getElementsByTagName('h1')[0];
-      expect(h1.innerHTML).to.equal('Users');
-      done();
-      window.close();
-    });
+  beforeEach((done) => {
+    index = fs.readFileSync('./src/index.html', "utf-8");
+    $ = cheerio.load(index);
+    console.log('Testing...');
+    done();
   });
+  it('should say h1 says sers', () => {
+    const h1 = $('h1').first().text();
+    expect(h1).to.equal('Users');
+    // jsdom.env(index, function(err, window) {
+    //   const h1 = window.document.getElementsByTagName('h1')[0];
+    //   expect(h1.innerHTML).to.equal('Users');
+    //   done();
+    //   window.close();
+    // });
+  });
+
   it('should have td with id users', () => {
-    const index = fs.readFileSync('./src/index.html', "utf-8");
-    const $ = cheerio.load(index);
     const users = $('tbody').attr('id');
-    const userList = $('thead th').length;
+    const userList = $('thead th');
     expect(users).to.equal('users');
-    expect(userList).to.equal(4);
+    expect(userList.length).to.be.at.least(3);
+    expect(userList.last().text()).to.equal('Email');
     // done();
 
     // jsdom.env(index, function(err, window) {
